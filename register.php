1954573937f9e15938e4d2ce71f2f038d5c451e4
Sananthu-47 <sananthu47@gmail.com>
2021-03-08 01:42:56 +0530

    Removed branch and department input filed from teachers register

--- register.php
+++ register.php
@@ -7,7 +7,7 @@
             <span class='text-capitalize'>Mange your college details</span>
         </div>
         <!-- Login body holds all the elments in it -->
-        <div class="login-body">
+        <div class="main-body d-flex justify-content-center align-items-center"">
         <!-- This holds the card structure of login page -->
             <div class="register-card card">
             <!-- Login header to show this is the login card -->
@@ -69,7 +69,7 @@
                         </select>
                     </div>
                     <!-- Branch -->
-                    <div class="my-3 d-md-flex justify-content-around align-items-center px-2">
+                    <div class="my-3 d-md-flex justify-content-around align-items-center px-2" id='branch-div'>
                         <label for="branch" class='mx-2 badge badge-dark d-inline'>Branch:</label>
                         <select id="branch" class='form-control input-login'>
                             <option value="0">Select your branch</option>
@@ -84,7 +84,7 @@
                         </select>
                     </div>
                     <!-- Department -->
-                    <div class="my-3 d-md-flex justify-content-around align-items-center px-2">
+                    <div class="my-3 d-md-flex justify-content-around align-items-center px-2" id='dept-div'>
                         <label for="department" class='mx-2 badge badge-dark d-inline'>Department:</label>
                         <select id="department" class='form-control input-login'>
                             <option value="0">Select your department</option>
@@ -117,124 +117,7 @@
             </div><!-- </login-card> -->
         </div><!--</login-body>-->
     </div><!-- </wrappper> -->
-    <script>
-    //Global variables
-    let role = 'student';
-
-//Change branch and get department on basis of branch
-    $('#branch').on('change',()=>{
-        if($('#branch').val() !== '0')
-        {
-            let branch = $('#branch').val();
-        $.ajax({
-            url : "process/get-department.php",
-            type : "POST",
-            data : {branch},
-            success : function(data)
-                {
-                    if(data !== '')
-                    {
-                        $('#department').html( "<option value='0'>Select your department</option>"+data);
-                    }else{
-                        $('#department').html( "<option value='0'>Select your department</option>");
-                    }
-                }
-            });
-        }
-    });
-
-// Check for confirm password
-$('#confirm-password').on('keyup',()=>{
-    let newPassword = $('#confirm-password').val();
-    let password = $('#register-password').val();
-    if(newPassword !== password)
-    {
-        $('#confirm-password').addClass('border border-danger');
-    }else{
-        $('#confirm-password').removeClass('border-danger');
-        $('#confirm-password').addClass('border-success');
-    }
-});
-
-//Select the role of teacher
-$('#teacher').on('click',(e)=>{
-    e.preventDefault();
-        $('#student').removeClass('selected');
-        $('#teacher').addClass('selected');
-        $('#class-div').hide();
-        $('#class-div').removeClass('d-md-flex');
-        role = 'teacher';
-    });
-//Select the role of student
-$('#student').on('click',(e)=>{
-    e.preventDefault();
-    $('#student').addClass('selected');
-    $('#teacher').removeClass('selected');
-    $('#class-div').show();
-    $('#class-div').addClass('d-md-flex');
-    role = 'student';
-});
-
-// Register user
-$('#register-form').on('submit',(e)=>{
-    e.preventDefault();
-    //Get all values to send to the register-user.php
-    let regId = $('#reg-id').val();
-    let username = $('#username').val();
-    let email = $('#email').val();
-    let institute = $('#institute').val();
-    let classSelected = $('#select-class').val();
-    let branch = $('#branch').val();
-    let department = $('#department').val();
-    let password = $('#register-password').val();
-    let confirmPassword = $('#confirm-password').val();
-    //If any data is not entered then it will show a alert to fill it properly
-    if(password === confirmPassword && regId !== '' && username !== '' && email !== '' && institute !== '' && branch !== 0 && department !== 0) 
-    {
-        if(role === 'student' && classSelected !== 0 || role === 'teacher')
-        {
-        $.ajax({
-            url : "process/register-user.php",
-            type : "POST",
-            data : {regId,username,email,institute,branch,department,password,role,classSelected},
-            success : function(data)
-                {
-                    const errorField = $('.disappear');//Get the error filed of to show the pop up messages
-                   if(data == 1)
-                   {
-                        $('.error-data').html(`User added successfully!`);
-                        errorField.addClass('success-field');//Add the sucess bg and color
-                        errorField.removeClass('danger-field');//Remove if exists red bg and color
-                        errorField.show();
-                        //To remove the alert message after 3sec
-                        setTimeout(() => {
-                            errorField.hide();
-                        }, 3000);
-                        document.getElementById('register-form').reset();//Reset form
-                        $('#register-password').val('123456');//Reset the password back to 123456
-                        $('#confirm-password').removeClass('border border-sucess');//Reset the border color
-                        $('#department').html( "<option value='0'>Select your department</option>");//Reset the department options
-                   }else{
-                        $('.error-data').html(data);
-                        errorField.addClass('danger-field');//Add red bg and color
-                        errorField.removeClass('success-field');//Remove if exists sucess bg and color
-                        errorField.show();
-                   }
-                }
-            });
-        }else{
-            alert('Select your class to proceed');
-        }
-    }else{
-        alert('Enter all field data properly');
-    }
-});
-
-//To close the pop up of error
-$('.close').on('click',()=>{
-    $('.disappear').hide();
-});
-
-    </script>
+    <!-- Script to send ajax data and all register related js file -->
+    <script src='assets/js/register-user.js'></script>
 
 <?php include "includes/fotter.php"; ?>
